61d7a1460e709406b9b473f3ccd852b6d4fcdf84
Rob-Yoo <jin12243@naver.com>
2024-12-14 14:15:22 +0900

    Feat: 백준 농구 경기

---
+++ Baekjoon/Baekjoon/Baekjoon/1주차 - 재귀, 구현/농구경기.swift
@@ -0,0 +1,25 @@
+//
+//  농구경기.swift
+//  Baekjoon
+//
+//  Created by Jinyoung Yoo on 12/14/24.
+//
+
+//let num = Int(readLine()!)!
+//var dict = [Character: Int]()
+//var result = ""
+//
+//for _ in 0..<num {
+//    let name = readLine()!
+//    let first = name[name.startIndex]
+//    dict[first, default: 0] += 1
+//}
+//
+//for (key, val) in dict.sorted(by: <) {
+//    if val >= 5 {
+//        result += String(key)
+//    }
+//}
+//
+//print(result.isEmpty ? "PREDAJA" : result)
+
