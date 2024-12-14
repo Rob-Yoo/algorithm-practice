8908c62fc7f0841c3546d9f44765fe18806921a2
Rob-Yoo <jin12243@naver.com>
2024-12-14 14:06:08 +0900

    Feat: 백준 펠린드롬 확인하기

--- Baekjoon/Baekjoon/Baekjoon/1주차 - 재귀, 구현/트럭주차.swift
+++ Baekjoon/Baekjoon/Baekjoon/1주차 - 재귀, 구현/트럭주차.swift
@@ -5,26 +5,26 @@
 //  Created by Jinyoung Yoo on 12/13/24.
 //
 
-let parkingFee = readLine()!.split(separator: " ").map { Int($0)! }
-let (A, B, C) = (parkingFee[0], parkingFee[1], parkingFee[2])
-var truckCountPerMinute = Array(repeating: 0, count: 101)
-var result = 0
-
-for _ in 1...3 {
-    let timeInfo = readLine()!.split(separator: " ").map { Int($0)! }
-    for time in (timeInfo[0] + 1)...timeInfo[1] {
-        truckCountPerMinute[time] += 1
-    }
-}
-
-for truckCount in truckCountPerMinute {
-    if (truckCount == 1) {
-        result += A
-    } else if (truckCount == 2) {
-        result += (B * 2)
-    } else if (truckCount == 3) {
-        result += (C * 3)
-    }
-}
-
-print(result)
+//let parkingFee = readLine()!.split(separator: " ").map { Int($0)! }
+//let (A, B, C) = (parkingFee[0], parkingFee[1], parkingFee[2])
+//var truckCountPerMinute = Array(repeating: 0, count: 101)
+//var result = 0
+//
+//for _ in 1...3 {
+//    let timeInfo = readLine()!.split(separator: " ").map { Int($0)! }
+//    for time in (timeInfo[0] + 1)...timeInfo[1] {
+//        truckCountPerMinute[time] += 1
+//    }
+//}
+//
+//for truckCount in truckCountPerMinute {
+//    if (truckCount == 1) {
+//        result += A
+//    } else if (truckCount == 2) {
+//        result += (B * 2)
+//    } else if (truckCount == 3) {
+//        result += (C * 3)
+//    }
+//}
+//
+//print(result)
---
+++ Baekjoon/Baekjoon/Baekjoon/1주차 - 재귀, 구현/펠린드롬 확인하기.swift
@@ -0,0 +1,22 @@
+//
+//  펠린드롬 확인하기.swift
+//  Baekjoon
+//
+//  Created by Jinyoung Yoo on 12/14/24.
+//
+
+//let word = readLine()!.map { String($0) }
+//var (left, right) = (0, word.count - 1)
+//var result = 1
+//
+//while left <= right {
+//    if (word[left] != word[right]) {
+//        result = 0
+//        break
+//    }
+//    left += 1
+//    right -= 1
+//}
+//
+//print(result)
+
