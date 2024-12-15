53f91134e15547a713e9f0b32bdbbfda552179b8
Rob-Yoo <jin12243@naver.com>
2024-12-15 14:10:06 +0900

    Feat: 백준 수열

---
+++ Baekjoon/Baekjoon/Baekjoon/1주차 - 재귀, 구현/수열.swift
@@ -0,0 +1,21 @@
+//
+//  수열.swift
+//  Baekjoon
+//
+//  Created by Jinyoung Yoo on 12/15/24.
+//
+
+//let first = readLine()!.split(separator: " ").map { Int($0)! }
+//let(N, K) = (first[0], first[1])
+//let days = readLine()!.split(separator: " ").map { Int($0)! }
+//var prefixSum = Array(repeating: 0, count: N + 1)
+//var result = Int.min
+//
+//for i in 1...N { prefixSum[i] = prefixSum[i - 1] + days[i - 1] }
+//
+//for i in 0...(N - K) {
+//    result = max(result, prefixSum[i + K] - prefixSum[i])
+//}
+//
+//print(result)
+
