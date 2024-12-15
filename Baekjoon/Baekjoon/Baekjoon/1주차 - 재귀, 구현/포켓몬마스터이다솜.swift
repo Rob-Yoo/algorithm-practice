337b5923fe8a8b8859e654b6c840d27d34b69356
Rob-Yoo <jin12243@naver.com>
2024-12-15 14:10:31 +0900

    Feat: 백준 포켓몬 마스터 이다솜

---
+++ Baekjoon/Baekjoon/Baekjoon/1주차 - 재귀, 구현/포켓몬마스터이다솜.swift
@@ -0,0 +1,31 @@
+//
+//  포켓몬마스터이다솜.swift
+//  Baekjoon
+//
+//  Created by Jinyoung Yoo on 12/15/24.
+//
+
+//let input = readLine()!.split(separator: " ").map { Int($0)! }
+//let (N, M) = (input[0], input[1])
+//var nameDict = [String: Int]()
+//var numberDict = [String: String]()
+//var quizs = [String]()
+//
+//for num in 1...N {
+//    let name = String(readLine()!)
+//    nameDict[name] = num
+//    numberDict[String(num)] = name
+//}
+//
+//for _ in 0..<M {
+//    let quiz = String(readLine()!)
+//    quizs.append(quiz)
+//}
+//
+//quizs.forEach {
+//    if let number = numberDict[$0] {
+//        print(number)
+//    } else if let name = nameDict[$0] {
+//        print(name)
+//    }
+//}
